b6c4920d1908a90f5bebba3f03e243f5769a22aa
Antzaz <31651836+Antzaz@users.noreply.github.com>
2026-08-11 21:53:21 +0300

    Expose six ML research models

---
+++ machine_learning/__init__.py
@@ -0,0 +1,14 @@
+from .common import MLResult
+from .models import (
+    ExpectedReturnModel,
+    EarningsSurpriseModel,
+    FinancialAnomalyModel,
+    MarketRegimeModel,
+    AIImpactMLModel,
+    PortfolioPositionSizingModel,
+)
+
+__all__=[
+    "MLResult","ExpectedReturnModel","EarningsSurpriseModel","FinancialAnomalyModel",
+    "MarketRegimeModel","AIImpactMLModel","PortfolioPositionSizingModel",
+]
